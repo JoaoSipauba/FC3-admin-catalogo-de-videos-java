2ca07289c60acd25650e7be6e15dbf269c2840fc
joao.sipauba <joao.sipauba@ceuma.br>
2023-01-31 20:09:57 -0300

    Criando a entidade category com TDD

---
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
@@ -0,0 +1,58 @@
+package com.fullcycle.admin.catalogo.domain.category;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public class Category {
+    private String id;
+    private String name;
+    private String description;
+    private Boolean isActive;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private Instant deletedAt;
+
+    private Category(String id, String name, String description, Boolean isActive, Instant createdAt, Instant updatedAt, Instant deletedAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.isActive = isActive;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+    }
+
+    public static Category newCategory(final String aName, final String aDescription, final Boolean isActive) {
+        final var id = UUID.randomUUID().toString();
+        final var now = Instant.now();
+        return new Category(id, aName, aDescription, isActive, now, now, null);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Instant getDeletedAt() {
+        return deletedAt;
+    }
+}
